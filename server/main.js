a0e94a8a80872ed597ce3dddf1305b481901fb56
markjconlon <markjconlon@gmail.com>
2017-11-02 13:03:34 -0400

    removed insecure package and had to write a Meteor.method in server/main.js for the client side to call

--- server/main.js
+++ server/main.js
@@ -2,4 +2,12 @@ import { Meteor } from 'meteor/meteor';
 Resolutions = new Mongo.Collection('resolutions');
 Meteor.startup(() => {
   // code to run on server at startup
+  Meteor.methods({
+    addResolution: function(title) {
+      Resolutions.insert({
+        title: title,
+        createdAt: new Date()
+      });
+    }
+  })
 });
