bf8313e3f80affad79cdc19fa7a20454029ad5d1
markjconlon <markjconlon@gmail.com>
2017-11-02 10:39:08 -0400

    added Template.Body.events function to listen for the form submission and inserting the resolution into mongo db

--- client/main.js
+++ client/main.js
@@ -9,3 +9,21 @@ Template.body.helpers({
       return Resolutions.find();
   }
 });
+
+Template.body.events({
+  // listens for the submit event on the .new-resolution form ex/ if it was click you would replace
+  // submit with click
+  "submit .new-resolution": function(event) {
+    var title = event.target.title.value;
+
+    Resolutions.insert({
+      title: title,
+      createdAt: new Date()
+    });
+
+    event.target.title.value = "";
+
+    // return false makes sure the page doesn't refresh
+    return false;
+  }
+});
