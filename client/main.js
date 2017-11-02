88ac5a7f147f00e4d89fce8d94e5a5b3207700ac
markjconlon <markjconlon@gmail.com>
2017-11-02 10:27:04 -0400

    switched over to using mongodb instead of an object in main.js, also had to declare the mongo collection on both client and server side.js

--- client/main.js
+++ client/main.js
@@ -1,10 +1,11 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
 
+Resolutions = new Mongo.Collection('resolutions');
+
 Template.body.helpers({
-  resolutions: [
-    { title: "Hello Resolution #1"}
-  ]
+  resolutions: function() {
+      return Resolutions.find();
+  }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-
+Resolutions = new Mongo.Collection('resolutions');
 Meteor.startup(() => {
   // code to run on server at startup
 });
